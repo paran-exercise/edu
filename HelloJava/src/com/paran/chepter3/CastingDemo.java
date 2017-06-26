2da861dcf82f86803065b8755c6dbc330465692d
2강008 <2008@192.168.0.133>
2017-06-26 10:46:26 +0900

    CastingDemo

---
+++ HelloJava/src/com/paran/chepter3/CastingDemo.java
@@ -0,0 +1,19 @@
+package com.paran.chepter3;
+
+
+public class CastingDemo {
+	 int num1;
+		long num2;
+		float num3;
+		
+		public static void printMsg(String msg){
+			
+			CastingDemo js = new CastingDemo();
+			
+			js.num1 = 10000000;
+			
+			System.out.println(js.num1);
+			
+			
+		}
+}
