d065f5ab00decedc802830ba2b91cc2f1f142657
OhSeongho <ethiopua03@gmail.com>
2017-06-26 11:17:12 +0900

    CastingDemo추가

---
+++ HelloJava/src/com/paran/chapter3/CastingDemo.java
@@ -0,0 +1,32 @@
+package com.paran.chapter3;
+
+public class CastingDemo {
+	
+	int num1;
+	long num2;
+	float num3;
+	
+	long num4;
+	float num5;
+	
+	public static void main(String[] args){
+		
+//		int num1 = 10000000000;
+//		long num2 = 10000000000;
+//		float num3 = 12.45;
+		
+		long num4 = 10000000000l;
+		float num5 =12.45f;
+		
+		CastingDemo obj = new CastingDemo();
+		
+		obj.num1 = 10000000000;
+		obj.num2 = 10000000000;
+		obj.num3 = 12.45;
+		
+		obj.num4 = 10000000000l;
+		obj.num5 = 12.45f;
+	}
+
+}
+
