018658cd3852129043cff0e54749436a4744e0e7
2강009 <2009@192.168.0.12>
2017-06-26 10:48:51 +0900

    이승주 CastingDemo

---
+++ HelloJava/src/com/paran/chapter3/CastingDemo.java
@@ -0,0 +1,23 @@
+package com.paran.chapter3;
+
+public class CastingDemo {
+
+	int num1;
+	static float num3;
+	
+	public static void main(String[] args) {
+		
+		CastingDemo de = new CastingDemo();
+		de.num1=10000000000; //인스턴스 변수
+		
+		
+		int num2 = 10000000000; // 멤버변수
+		
+		num3 = 12.45; // 클래스변수
+		
+	
+
+	}
+			
+	
+}
