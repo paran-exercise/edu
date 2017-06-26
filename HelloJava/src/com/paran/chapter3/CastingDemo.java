5e15b51d71fb9132bbb01ee68620f363ec637ca1
2강009 <2009@192.168.0.12>
2017-06-26 10:55:44 +0900

    이승주 Change

--- HelloJava/src/com/paran/chapter3/CastingDemo.java
+++ HelloJava/src/com/paran/chapter3/CastingDemo.java
@@ -3,19 +3,17 @@ package com.paran.chapter3;
 public class CastingDemo {
 
 	int num1;
-	static float num3;
+	long num2;
+	float num3;
 	
 	public static void main(String[] args) {
 		
 		CastingDemo de = new CastingDemo();
-		de.num1=10000000000; 
 		
-		
-		int num2 = 10000000000; 
-		
-		num3 = 12.45;
-		
-	
+		de.num1=10000000000;		
+		de.num2=10000000000;		
+		de.num3=12.45;
+					
 
 	}
 			
