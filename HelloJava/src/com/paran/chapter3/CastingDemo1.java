f4bed54526818fc4fd8c9d57b6b2a3437e478a12
Administrator <Administrator@USER-10>
2017-06-26 17:33:28 +0900

    복합연산자 테스트 01

--- HelloJava/src/com/paran/chapter3/CastingDemo1.java
+++ HelloJava/src/com/paran/chapter3/CastingDemo1.java
@@ -34,3 +34,56 @@ public class CastingDemo1 {
 	}
 }
 
+
+	class StudentScore{
+		int engScore;
+		int korsocre;
+		String stuName;
+	
+	public StudentScore(String stuName,int korsocre,int engScore){
+		this.stuName = stuName;
+		this.korsocre = korsocre;
+		this.engScore = engScore;
+	}
+	
+	public void showInfo(){
+		System.out.println("=====정보출력 시작 =======");
+		System.out.println("이름"+stuName);
+		System.out.println("국어점수"+korsocre);
+		System.out.println("영어점수"+engScore);
+	}
+	
+	public double getAvg(){
+		return (engScore+korsocre)/2; 
+	}	
+}
+
+
+// 3세람   90점 이상 " A " 인 사람만 정보를 출력해 주자. 90<= "A" , 
+
+class OperatorDemo{
+	public static void main(String[] args){
+
+		StudentScore obj1 = new StudentScore("홍길동",100,80);
+		StudentScore obj2 = new StudentScore("임꺽정",50,40);
+		StudentScore obj3 = new StudentScore("변사또",80,80);
+		
+		obj1.showInfo();
+		obj2.showInfo();
+		obj3.showInfo();
+		
+		if (obj1.getAvg()>= 90.0 && obj1.getAvg()<=100.0){
+			obj1.showInfo();
+		}
+	
+		if (obj1.getAvg()>= 90.0 && obj2.getAvg()<=100.0){
+			obj1.showInfo();
+		}
+		
+		if (obj1.getAvg()>= 90.0 && obj3.getAvg()<=100.0){
+			obj1.showInfo();
+	}
+
+}
+	
+}
---
+++ HelloJava/src/com/paran/chapter3/demo3.java
@@ -0,0 +1,21 @@
+package com.paran.chapter3;
+
+public class demo3 {
+
+	public static void main(String[]args){
+		
+	
+	
+	int a = 1;
+	int b = 2; 
+
+	a+=1+2;
+	
+	System.out.println(a);
+	System.out.println(b);
+
+			
+	}
+}
+
+
