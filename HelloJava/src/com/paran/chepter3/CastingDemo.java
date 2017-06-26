b6ea024088713e3c8ae8e35d41e5c902bf6e9e44
2강008 <2008@192.168.0.133>
2017-06-26 11:16:15 +0900

    CastingDemo 2차

--- HelloJava/src/com/paran/chepter3/CastingDemo.java
+++ HelloJava/src/com/paran/chepter3/CastingDemo.java
@@ -2,18 +2,24 @@ package com.paran.chepter3;
 
 
 public class CastingDemo {
-	 int num1;
-		long num2;
-		float num3;
+	int num1;
+	long num2;
+	float num3;
 		
 		public static void main(String[] args){
 			
 			CastingDemo js = new CastingDemo();
+		
+			js.num1 = 10000000000;
+			js.num2 = 10000000000;
+			js.num3 = 12.45;
 			
-			js.num1 = 10000000;
+			
 			
 			System.out.println(js.num1);
-			 
+			System.out.println(js.num2);
+			System.out.println(js.num3);
+			  
 			
 		}
 }
