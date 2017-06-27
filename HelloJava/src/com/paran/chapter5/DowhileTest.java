152a31985fce39ed364729671e82967ac795c781
Administrator <Administrator@USER-10>
2017-06-27 11:57:32 +0900

    오늘 학습한 것들.. for 문 + array + 지역변수 +  this + 메서드 호출를 사용하여
    조건문을 통해 [학번,이름,제대여부]  리스트 출력

---
+++ HelloJava/src/com/paran/chapter5/DowhileTest.java
@@ -0,0 +1,24 @@
+package com.paran.chapter5;
+
+
+
+public class DowhileTest {
+
+	
+	
+	public static void main(String[] args) 
+	{
+	
+		int num=0;
+		
+		do
+		{
+			System.out.println("I like java"+num);
+			num++;
+			
+		} while(num<7);
+		
+
+	}
+
+}
