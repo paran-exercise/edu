de018dbdbc39b7bfb0c8761c9b6546e35632beed
2강008 <2008@192.168.0.133>
2017-06-29 17:15:29 +0900

    Array 최종 수정

--- HelloJava/src/com/paran/chapter13/ArrayDemo.java
+++ HelloJava/src/com/paran/chapter13/ArrayDemo.java
@@ -1,29 +1,69 @@
 package com.paran.chapter13;
+import java.util.Arrays;
 public class ArrayDemo {
 	
-	
-public static void  Array(int[] ar ){
-		for(int i = 0;  i < ar.length; i++){
-			ar[i] =  i + 1;
-			System.out.println(ar[i]);
-		}	
+	int member;
+	int[][] dataMatrixArr; 
+//	static final int FIRST_DIM_SIZE = 2;
+//	static final int SECOND_DIM_SIZE = 2;
+	public void initMatrixArr(int firstDimSize, int secondDimSize){
+		dataMatrixArr = new int[firstDimSize][secondDimSize];
+		
+		int incrementValue = 0;
+		for(int i = 0; i <dataMatrixArr.length; i++){
+			for(int j = 0; j < dataMatrixArr[i].length; j++){
+				dataMatrixArr[i][j] = ++incrementValue;
+				
+				
+			}
+		}
 	}
-public static void  Array2(int[] ar ){
-	for(int i = 0;  i < ar.length; i++){
-		ar[i] = 10 - i ;
-		System.out.println(ar[i]);
-	}	
+	
+	
+	public void transformArrSize(){
+		dataMatrixArr[3] = new int[]{0,0,0,0,0,0,0,0};
+	}
+	
+	
+	
+	 
+	
+	
+	/**
+	 * @author 2강008
+	 * @param array
+	 * @return void
+	 */
+public static void reverseArrayValue(int[] array){
+	int temp = 0, front = 0, rear =  array.length-1;
+	for(;front < array.length /2; front++,rear-- ){
+		temp = array[front];
+		array[front] = array[rear];
+		array[rear] = temp;
+	}
 }
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int[] js  = new int[10];
-		Array(js);
-		System.out.println("  ");
-		System.out.println("  ");
-		System.out.println("  ");
-		System.out.println("  ");
-		System.out.println("  ");
-		System.out.println("  ");
-		Array2(js);
-}
-}
+public static void main(String[] args) {
+
+	ArrayDemo obj = new ArrayDemo();
+	
+	obj.initMatrixArr(4, 4);
+	System.out.println("31324");
+	obj.transformArrSize();
+	
+	
+		int[] reverseArr  = {1,2,3,4,5,6,7,8,9,10};
+//		reverseArrayValue(reverseArr);
+//		for(int val : reverseArr){
+//			System.out.println(val);
+//		}
+		
+		
+		Arrays.sort(reverseArr);
+		
+		for(int i = 0 ; i < reverseArr.length; i++){
+			System.out.println(reverseArr[i]);
+		}
+		
+		
+	}
+}	
