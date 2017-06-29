fcbc108fb4b81c02d24802ff16d92a33927f07df
2강008 <2008@192.168.0.133>
2017-06-29 14:18:09 +0900

    ArrayDemo

--- HelloJava/src/com/paran/chapter13/ArrayDemo.java
+++ HelloJava/src/com/paran/chapter13/ArrayDemo.java
@@ -1,35 +1,29 @@
 package com.paran.chapter13;
-
-
-
 public class ArrayDemo {
 	
 	
-	public static void  Array(int[] ar ){
+public static void  Array(int[] ar ){
 		for(int i = 0;  i < ar.length; i++){
-			ar[i] = 10 - i ;
+			ar[i] =  i + 1;
 			System.out.println(ar[i]);
-		}
-		
+		}	
 	}
-
+public static void  Array2(int[] ar ){
+	for(int i = 0;  i < ar.length; i++){
+		ar[i] = 10 - i ;
+		System.out.println(ar[i]);
+	}	
+}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		
 		int[] js  = new int[10];
 		Array(js);
-		
-		
-			
-		
-		
-		
-			}
-
+		System.out.println("  ");
+		System.out.println("  ");
+		System.out.println("  ");
+		System.out.println("  ");
+		System.out.println("  ");
+		System.out.println("  ");
+		Array2(js);
 }
-		
-		
-	
-
-
+}
