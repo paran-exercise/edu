e4322e71426fcd5a0c54b5b611cef7c67de47bfa
2강013 <2013@192.168.0.143>
2017-06-29 15:26:32 +0900

    ArrayDemo 수정

--- HelloJava/src/com/paran/chapter13/ArrayDemo.java
+++ HelloJava/src/com/paran/chapter13/ArrayDemo.java
@@ -2,26 +2,26 @@ package com.paran.chapter13;
 
 public class ArrayDemo {
 	  public static void main(String[] args) {
-		  int[] Array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		  int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 		  System.out.println("변경 전");
-		  for(int i = 0; i < Array.length; i++) {
-			  System.out.print(Array[i] + " ");
+		  for(int i = 0; i < array.length; i++) {
+			  System.out.print(array[i] + " ");
 		  }
       
-		  reverseArrayValue(Array);
+		  reverseArrayValue(array);
 		  System.out.println("\n변경 후");
-		  for(int i = 0; i < Array.length; i++) {
-			  System.out.print(Array[i] + " ");
+		  for(int i = 0; i < array.length; i++) {
+			  System.out.print(array[i] + " ");
 		  }
 	  }
-	  public static void reverseArrayValue(int[] Arr) {
+	  public static void reverseArrayValue(int[] arr) {
 		  int swapPlace = 0;
-		  for(int i = 0; i < Arr.length; i++) {
-			  for(int y = i+1; y < Arr.length; y++) {
-				  if(Arr[i] < Arr[y]) {
-					  swapPlace = Arr[y];
-					  Arr[y] = Arr[i];
-					  Arr[i] = swapPlace;
+		  for(int i = 0; i < arr.length; i++) {
+			  for(int y = i+1; y < arr.length; y++) {
+				  if(arr[i] < arr[y]) {
+					  swapPlace = arr[y];
+					  arr[y] = arr[i];
+					  arr[i] = swapPlace;
 				  }
 			  }
 		  }
