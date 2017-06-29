66b20cc7ab42c2c4cbbd30991142cff0c5e24040
2강020 <2020@192.168.0.142>
2017-06-29 14:37:42 +0900

    ArrayDemo

--- HelloJava/src/com/paran/chapter13/ArrayDemo.java
+++ HelloJava/src/com/paran/chapter13/ArrayDemo.java
@@ -1,23 +1,29 @@
 package com.paran.chapter13;
 
 public class ArrayDemo {
-	public static void main(){
-		int[] array = {1,2,3,4,5,6,7,8,9,10};
-		reverseArrayValue(array);
-		for(int i:array){
-			System.out.println(i);
-		}
-	}
-	static void reverseArrayValue(int[] array){
-		int i=0, s, end=array.length-1;
+	public static void main(String[] args){
+		int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 		
-		while(i<array.length/2){
-			s = array[i];
-			array[i] = array[end];
-			array[end] = s;
-			i++;
-			end--;
+		for(int i:array){
+			System.out.print(i);
+		}
+		PlayWithArray.reverseArrayValue(array);
+		System.out.print('\n');
+		for(int i:array){
+			System.out.print(i);
 		}
 	}
 }
-// call by value, call by reference
+class PlayWithArray{
+	static void reverseArrayValue(int[] array){
+		int i=0, s, j=array.length-1;
+		
+		while(i<array.length/2){
+			s = array[i];
+			array[i] = array[j];
+			array[j] = s;
+			i++;
+			j--;
+		}
+	}
+}
