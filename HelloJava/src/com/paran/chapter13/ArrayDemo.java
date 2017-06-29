9fd98f6ae8af17a33e8c498f2bca07ce5c0533a8
2강009 <2009@192.168.0.15>
2017-06-29 15:29:11 +0900

    오름내림

--- HelloJava/src/com/paran/chapter13/ArrayDemo.java
+++ HelloJava/src/com/paran/chapter13/ArrayDemo.java
@@ -1,50 +1,119 @@
 package com.paran.chapter13;
 
+import java.util.Scanner;
+
 public class ArrayDemo {
 	
 	
 	public static void main(String[] args) {
 		
 		ArrayDemo arr = new ArrayDemo();
-		int[] num = new int[10];
+		//int[] num = new int[10];
+		
+		int num[] = {1,4,6,2,8,3,5,7,9,10};
 				
-		for(int i = 0;i<num.length;i++){
-			num[i]=i+1;
-			System.out.print(num[i]+"\t");
+//		for(int i = 0;i < num.length;i++){
+//			num[i]=i+1;
+//			//System.out.print(num[i]+"\t");
+//			
+//		}
+		//System.out.println();
+		//arr.reverseArrayValue(num);
+		
+		System.out.println("내림차순 : 1\n오름차순 : 2");
+		
+		Scanner sc = new Scanner(System.in);
+		int choise = sc.nextInt();
+		
+		switch(choise){
+		
+		case 1: {
+		ArrayDemo.descendingOrder(num); //내림차순
+		break;
+		}
+		case 2: {
+		ArrayDemo.ascendingOrder(num);  //오름차순
+		break;
+		}
+		default : {
+			System.out.println("잘못입력했어요");
 			
 		}
-		System.out.println();
-		arr.reverseArrayValue(num);
 		
 		
 	}
+}
 	
 	
 	public void reverseArrayValue(int[] array){
 		
 		int temp=10;
-//		int temp;	
+		
 		
 		for(int i = 0; i < array.length; i++){      
-			
-//		 for(int j = 0; j < array.length; j++){
+					
 							
 				array[i]=temp;
 				temp--;
+				System.out.print(array[i]+"\t");
+		 }
+			
+		 
+			
+	}
+		
+	
+	
+	public static void descendingOrder(int[] array){
+		
+		int temp;	
+		
+		for(int i = 0; i < array.length; i++){      
+			
+		  for(int j = i+1; j < array.length; j++){
+							
 				
-//				if(array[i]<array[j]){
-//					temp=array[i];
-//					array[i]=array[j];
-//					array[j]=temp;
-//					
-//				}	
+				if(array[i]<array[j]){
+					temp=array[i];
+					array[i]=array[j];
+					array[j]=temp;
+					
+				}	
 				
-//		 }
+		 }
 			
 		 System.out.print(array[i]+"\t");
 			
 		}
+		
 	}
+	
+	
+	
+	public static void ascendingOrder(int[] array){
+		
+		int temp;	
+		
+		for(int i = 0; i < array.length; i++){      
+			
+		  for(int j = i+1; j < array.length; j++){
+							
+				
+				if(array[i]>array[j]){
+					temp=array[j];
+					array[j]=array[i];
+					array[i]=temp;
+					
+				}	
+				
+		 }
+			
+		 System.out.print(array[i]+"\t");
+			
+		}
+				
+	}	
+	
 }
 		
 		
