d75884dd606f5a34fc5d0043451ab9755e35d3c7
OhSeongho <ethiopua03@gmail.com>
2017-06-29 14:33:39 +0900

    오름차순/내림차순 추가

--- HelloJava/src/com/paran/chapter13/ArrayDemo.java
+++ HelloJava/src/com/paran/chapter13/ArrayDemo.java
@@ -8,34 +8,51 @@ public class ArrayDemo {
 		for(int i=0; i < beforeArray.length ; i++){
 			beforeArray[i]=i+1;
 		}
+		
 		for(int i=0;i < beforeArray.length;i++){
-			System.out.print(beforeArray[i]+" ");
-			
-			
+			System.out.print(beforeArray[i]+" ");			
 		}
-		System.out.println();
 		
-		reverseValue(beforeArray);
+		System.out.println("\n");
 		
+		reverseArrValue(beforeArray);
+		
+		sortArrValue(beforeArray,1);
+		
+		System.out.println("\n");
 		for(int i=0;i < beforeArray.length;i++){
-			System.out.print(beforeArray[i]+" ");
+			System.out.print(beforeArray[i]+" ");			
+		}
+		
+	}
+	
+	static void reverseArrValue(int[] Array){
+		for(int i=Array.length-1;i>=0;i--){
+			System.out.print(Array[i]+" ");
 		}
 	}
 	
-	static void reverseValue(int[] Array){
-		
+	static void sortArrValue(int[] Array, int ascendingDecending){
 		int buffer=0;
 		for(int i=0; i < Array.length-1; i++){
 			for(int j=0; j < Array.length-1; j++){
-				if(Array[j] < Array[j+1]){
-					buffer = Array[j];
-					Array[j] = Array[j+1];
-					Array[j+1] = buffer;
+				if(ascendingDecending == 1){
+					if(Array[j] < Array[j+1]){
+						buffer = Array[j];
+						Array[j] = Array[j+1];
+						Array[j+1] = buffer;
+					}
+				}else{
+					if(Array[j] > Array[j+1]){
+						buffer = Array[j+1];
+						Array[j+1] = Array[j];
+						Array[j] = buffer;
+					}
 				}
+				
 			}			
 		}
 	}
-	
 }
 //int 배열 크기 10, 초기값 1~10할당
 //return type : void
