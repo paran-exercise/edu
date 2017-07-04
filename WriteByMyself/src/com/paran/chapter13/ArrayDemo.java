e9f64e090ee2d6e74d170b6104eecba90175f343
2강020 <2020@192.168.0.142>
2017-07-04 17:41:45 +0900

    미완성

--- WriteByMyself/src/com/paran/chapter13/ArrayDemo.java
+++ WriteByMyself/src/com/paran/chapter13/ArrayDemo.java
@@ -6,8 +6,23 @@ import java.io.IOException;
 
 public class ArrayDemo {
 	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new FileReader("c:/dev/asd.txt"));
-		String line = br.readLine();
-		System.out.println(line.substring(14, 27));
+//		BufferedReader br = new BufferedReader(new FileReader("c:/dev/asdq.txt"));
+//		String line = br.readLine();
+//		String[] splitLine;
+//		splitLine = line.split("_");
+//		System.out.println(splitLine[3].equals("Fore"));
+//	
+		BufferedReader br = new BufferedReader(new FileReader("c:/dev/asdq.txt"));
+		int lineCount=0;
+		String line;
+		String[] splitLine = new String[10]; 		
+		
+		while((line = br.readLine()) != null){
+			splitLine = line.split("_");
+			
+			if(splitLine[3].equals("Fore")){
+				System.out.println(splitLine[3]);
+			}
+		}
 	}
 }
--- WriteByMyself/src/com/paran/chapter13/ClassInfo.java
+++ WriteByMyself/src/com/paran/chapter13/ClassInfo.java
@@ -2,55 +2,61 @@ package com.paran.chapter13;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.ArrayList;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class ClassInfo {
 	static ArrayList<Student> stuList = new ArrayList<Student>();
-	
-	final static int NAME_LEN = 12;
-	final static int STUID_LEN = 23;
-	final static int RESIID_LEN = 37;
-	final static int RESI_LEN = 42;
-	
+
 	public static void main(String[] args)throws IOException{
-		BufferedReader br = new BufferedReader(new FileReader("c:/dev/asd.txt"));
+		BufferedReader br = new BufferedReader(new FileReader("c:/dev/asdq.txt"));
+		int lineCount=0;
+		String line;
+		String[] splitLine = new String[10]; 		
 		
-		for(int i=0; i<8; i++){
-			String line = br.readLine();
-			if((line.substring(RESIID_LEN+1, RESI_LEN)).equals("¿Ü±¹ÀÎ"))
-				stuList.add(new ForeStudent(line.substring(STUID_LEN, RESIID_LEN)));
-			else
-				stuList.add(new DomeStudent(line.substring(STUID_LEN, RESIID_LEN)));
+		while((line = br.readLine()) != null){
+			splitLine = line.split("_");
+			
+			if(splitLine[3].equals("Fore")){
+				stuList.add(new ForeStudent(splitLine[4]));
+			}else{
+				stuList.add(new DomeStudent(splitLine[4]));
+			}
+			lineCount++;
 		}
 		
 		br.close();
 		br = new BufferedReader(new FileReader("c:/dev/asd.txt"));
 		
-		for(int i=0; i<8; i++){		
-			String line = br.readLine();
-            (stuList.get(i)).setName(line.substring(0, NAME_LEN));
-            (stuList.get(i)).setStuID(line.substring(NAME_LEN, STUID_LEN));
-            (stuList.get(i)).setStuID(line.substring(RESIID_LEN));
-            
+		for(int i=0; (line = br.readLine()) != null; i++){		
+			splitLine = line.split("_");
+			
+			stuList.get(i).setName(splitLine[0]);
+            stuList.get(i).setStuID(splitLine[1]);
+            stuList.get(i).avgScore(splitLine[4]);
+            stuList.get(i).resi = splitLine[2];
 		}
 		br.close();
 		
 		
-		
-		for(int i=0; i<8; i++){
+		Collections.sort(stuList);
+		for(int i=0; i<lineCount; i++){
 			(stuList.get(i)).showInfo();
 		}
-	}
+	}	
 }
 
 //=======================abstract==============================
-abstract class Student{
+abstract class Student implements Comparable<Student>{
 	String name;
 	String stuID;
+	String resi;
+	
 	int engScore;
 	int korScore;
 	int proScore;
+	int avg;
 	
 	public void setName(String name){
 		this.name = name;
@@ -58,7 +64,15 @@ abstract class Student{
 	public void setStuID(String stuID){
 		this.stuID = stuID;
 	}
-	
+	public void avgScore(String scores){
+		this.engScore = Integer.parseInt(scores.substring(0, 2));
+		this.korScore = Integer.parseInt(scores.substring(2, 4));
+		this.proScore = Integer.parseInt(scores.substring(4, 6));
+		this.avg = (engScore+korScore+proScore)/3; 
+	}
+	public int compareTo(Student stu){
+		return this.avg - stu.avg;
+	}
 	public abstract void showInfo();
 }
 //=======================abstract==============================
@@ -70,7 +84,7 @@ class DomeStudent extends Student{
 		resiID = ID;
 	}	
 	public void showInfo(){
-		System.out.println("name =>"+name+" stuID =>"+stuID+" resiID =>"+resiID);
+		System.out.println("name =>"+name+" stuID =>"+stuID+" resiID =>"+resiID+" resi =>"+resi+" Average =>"+avg);
 	}	
 }
 
@@ -81,6 +95,6 @@ class ForeStudent extends Student{
 		foreignID = ID;
 	}
 	public void showInfo(){
-		System.out.println("name =>"+name+" stuID =>"+stuID+" foreignID =>"+foreignID);
+		System.out.println("name =>"+name+" stuID =>"+stuID+" foreignID =>"+foreignID+" resi =>"+resi+" Average =>"+avg);
 	}	
 }
